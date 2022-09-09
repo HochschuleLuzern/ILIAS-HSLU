6d267e6c432a86a51f72050d50ef3a79731aa63f
Stephan Kergomard <webmaster@kergomard.ch>
2022-09-09 11:28:38 +0300

    HSLU: Add floor and round to EvalMath

--- components/ILIAS/Math/classes/class.EvalMath.php
+++ components/ILIAS/Math/classes/class.EvalMath.php
@@ -98,6 +98,9 @@ class EvalMath
         'sin','sinh','arcsin','asin','arcsinh','asinh',
         'cos','cosh','arccos','acos','arccosh','acosh',
         'tan','tanh','arctan','atan','arctanh','atanh',
+        // BEGIN PATCH HSLU: Adding floor and round
+        'floor', 'round',
+        // END PATCH HSLU: Adding floor and round
         'sqrt','abs','ln','log');
 
     public function __construct()

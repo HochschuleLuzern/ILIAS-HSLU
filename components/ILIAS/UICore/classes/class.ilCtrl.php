6f4187c6b8ee7031288f9482cc6e1e8ee8392efd
msalterhslu <mark.salter@hslu.ch>
2023-06-07 11:23:17 +0200

    HSLU: Restore the public getTargetScript method in ilCtrl
    It was available pre 8.0.

--- components/ILIAS/UICore/classes/class.ilCtrl.php
+++ components/ILIAS/UICore/classes/class.ilCtrl.php
@@ -562,6 +562,14 @@ class ilCtrl implements ilCtrlInterface
     /**
      * @inheritDoc
      */
+    public function getTargetScript(): string
+    {
+        return $this->context->getTargetScript();
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function isAsynch(): bool
     {
         return $this->context->isAsync();

14c18579707ef2dffccdc15d05a676fc6b57e80f
msalterhslu <mark.salter@hslu.ch>
2023-09-12 11:33:43 +0200

    HSLU FIX: Correct wizard options delete error

--- components/ILIAS/soap/classes/class.ilSoapObjectAdministration.php
+++ components/ILIAS/soap/classes/class.ilSoapObjectAdministration.php
@@ -1180,7 +1180,7 @@ class ilSoapObjectAdministration extends ilSoapAdministration
         $wizard_options->saveRoot($source_id);
 
         foreach ($options as $source_id => $option) {
-            $wizard_options->addEntry($source_id, $option);
+            $wizard_options->addEntry(intval($source_id), $option);
         }
         $wizard_options->read();
 

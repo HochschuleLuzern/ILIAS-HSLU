b162c239bf4d2bf61481c934ee344f98520189f7
msalterhslu <mark.salter@hslu.ch>
2024-03-11 16:09:59 +0100

    HSLU: HSLU ext_account may not be blank

--- components/ILIAS/OpenIdConnect/classes/class.ilAuthProviderOpenIdConnect.php
+++ components/ILIAS/OpenIdConnect/classes/class.ilAuthProviderOpenIdConnect.php
@@ -148,6 +148,14 @@ class ilAuthProviderOpenIdConnect extends ilAuthProvider
 
         $this->logger->debug('Authenticated external account: ' . $ext_account);
 
+        //BEGIN PATCH HSLU ext_account may not be blank
+        if (!is_string($ext_account) || empty($ext_account)) {
+            $status->setStatus(ilAuthStatus::STATUS_AUTHENTICATION_FAILED);
+            $status->setReason('err_no_eduid');
+            return $status;
+        }
+        //END PATCH HSLU ext_account may not be blank
+
         $int_account = ilObjUser::_checkExternalAuthAccount(
             ilOpenIdConnectUserSync::AUTH_MODE,
             $ext_account

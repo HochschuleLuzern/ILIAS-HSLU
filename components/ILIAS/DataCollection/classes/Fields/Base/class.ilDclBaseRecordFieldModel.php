0bbdb8811df319dd4c79ea09f87622448c83ea68
msalterhslu <mark.salter@hslu.ch>
2024-04-08 15:05:30 +0200

    HSLU FIX: Fix for "No access rights" error message after cloning a course
    Jira: IL-285

--- components/ILIAS/DataCollection/classes/Fields/Base/class.ilDclBaseRecordFieldModel.php
+++ components/ILIAS/DataCollection/classes/Fields/Base/class.ilDclBaseRecordFieldModel.php
@@ -114,7 +114,11 @@ class ilDclBaseRecordFieldModel
 
             $next_id = $this->db->nextId("il_dcl_stloc" . $storage_location . "_value");
 
-            $value = $this->serializeData($this->value);
+            if ($storage_location == 3 && ($this->value == '0000-00-00 00:00:00' || empty($this->value))) {
+                $value = null;
+            } else {
+                $value = $this->serializeData($this->value);
+            }
 
             if (empty($this->getId())) {
                 $this->doCreate();

23d568571835e9c8b5b0cc48051bc9c0fe1a113d
msalterhslu <mark.salter@hslu.ch>
2024-10-01 11:43:22 +0200

    HSLU FIX: use case where role description may be missing

--- Services/User/classes/class.ilRoleAssignmentTableGUI.php
+++ Services/User/classes/class.ilRoleAssignmentTableGUI.php
@@ -204,7 +204,7 @@ class ilRoleAssignmentTableGUI extends ilTable2GUI
                 if (!isset($parent_node['type']) || !isset($parent_node['obj_id'])) {
                     continue;
                 }
-                $role['description'] = "{$this->lng->txt("obj_{$parent_node['type']}")}&nbsp;(#{$parent_node['obj_id']})";
+                $role["description"] = array_key_exists('type',$parent_node) ? $this->lng->txt("obj_" . $parent_node["type"]) . "&nbsp;(#" . $parent_node["obj_id"] . ")" : "(Unknown)";
             }
 
             $role_ids[$counter] = $role["obj_id"];

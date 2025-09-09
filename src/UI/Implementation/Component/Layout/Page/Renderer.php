485001d9700fe1e692fd716f2d9250cb5e0cbed4
msalterhslu <mark.salter@hslu.ch>
2025-09-09 12:01:47 +0200

    HSLU FIX: Hack to suppress accidental removal of LiveVoting latex variables in braces
    Jira: IL-580
    Mantis: 45749

--- src/UI/Implementation/Component/Layout/Page/Renderer.php
+++ src/UI/Implementation/Component/Layout/Page/Renderer.php
@@ -115,7 +115,16 @@ class Renderer extends AbstractComponentRenderer
             $component->getMetaData()
         )->toHtml());
 
-        return $tpl->get();
+        // START TEMP PATCH HSLU: Hack to suppress accidental removal of LiveVoting latex variables in braces
+        $output = $tpl->get();
+        global $DIC;
+        $cmdClass = $DIC->ctrl()->getCmdClass();
+        if ($cmdClass !== null && $cmdClass == 'ilobjlivevotinggui') {
+            $output = str_replace("&lbrace;","{",str_replace("&rbrace;","}",$output));
+            $output = str_replace("&amp;lbrace;","{",str_replace("&amp;rbrace;","}",$output));
+        }
+        return $output;
+        // END TEMP PATCH HSLU: Hack to suppress accidental removal of LiveVoting latex variables in braces
     }
 
     protected function convertBreadcrumbsToDropdownLocator(

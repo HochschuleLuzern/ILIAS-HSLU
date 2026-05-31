5c0878192b32e8ca887ea7b4a1c5047499dd9707
Katalin Lukacsne Toth <katalin.lukacsne@hslu.ch>
2026-05-31 17:13:48 +0200

    HSLU: move MathJax under public/Customizing
    Otherwise it gets deleted by composer.

--- hslu/get_mathjax.sh
+++ hslu/get_mathjax.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-mkdir public/mathjax
-cd public/mathjax
+mkdir -p public/Customizing/mathjax
+cd public/Customizing/mathjax
 
 # 3.2.2 is the last v3 release, couldn't get it work with v4 yet
 version=3.2.2
@@ -13,4 +13,4 @@ curl -sSL -o - https://github.com/mathjax/MathJax/archive/refs/tags/$version.tar
     tar xzf - --strip-components=2 MathJax-$version/es5
 
 # The URL to use in ILIAS config with this script:
-# https://elearning.hslu.ch/ilias/mathjax/tex-mml-chtml.js
+# https://elearning.hslu.ch/ilias/Customizing/mathjax/tex-mml-chtml.js

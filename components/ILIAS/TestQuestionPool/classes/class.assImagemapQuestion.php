e5810e5132d94fa32652210e661a31c52744cc18
Katalin Lukacsne Toth <katalin.lukacsne@hslu.ch>
2026-07-20 16:57:42 +0200

    HSLU FIX: Fix log call in Imagemap question
    Jira: IL-629

--- components/ILIAS/TestQuestionPool/classes/class.assImagemapQuestion.php
+++ components/ILIAS/TestQuestionPool/classes/class.assImagemapQuestion.php
@@ -304,7 +304,7 @@ class assImagemapQuestion extends assQuestion implements ilObjQuestionScoringAdj
                 $this->tpl->setOnScreenMessage('failure', 'The image could not be uploaded!');
                 return;
             }
-            $this->log->write('gespeichert: ' . $imagepath . $image_filename);
+            $this->log->root()->debug('gespeichert: ' . $imagepath . $image_filename);
         }
     }
 
